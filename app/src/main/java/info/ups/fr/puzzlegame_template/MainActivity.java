5d255eb0b4fa809426a241bb81a4acb378ec191e
bouletator <clement.romanet@gmail.com>
2015-03-27 09:33:30 +0100

    Création du lien entre le bouton start et GameActivity

--- app/src/main/java/info/ups/fr/puzzlegame_template/MainActivity.java
+++ app/src/main/java/info/ups/fr/puzzlegame_template/MainActivity.java
@@ -1,9 +1,12 @@
 package info.ups.fr.puzzlegame_template;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -11,7 +14,17 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.menu_application);
+
+        final Button loginButton = (Button) findViewById(R.id.start);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
